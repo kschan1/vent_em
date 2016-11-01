15f1043e05e031426309a07d7d2a806bd7698268
CKS <ckseng86@gmail.com>
2016-11-02 09:43:08 +1100

    add vent and user model

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require_relative 'db_config'
+require_relative 'models/user'
+require_relative 'models/vent'
 require 'pry'
 
+enable :sessions
+
+
 get '/' do
-  'hello world'
+  @vents = Vent.all
+  erb :index
 end
---
+++ vent_em.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE vent_em;
+
+CREATE TABLE vents (
+  id SERIAL4 PRIMARY KEY,
+  body VARCHAR(140),
+  user_id INTEGER
+);
+
+CREATE TABLE comments (
+  id SERIAL4 PRIMARY KEY,
+  body VARCHAR(140),
+  vent_id INTEGER
+);
+
+CREATE TABLE users (
+  id SERIAL4 PRIMARY KEY,
+  email VARCHAR(300),
+  username VARCHAR(200),
+  password_digest VARCHAR (400)
+);
