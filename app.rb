ed860aa4617d4bc994e8c24321c967b922443295
CKS <ckseng86@gmail.com>
2016-11-02 11:19:48 +1100

    add session, login and logout

--- app.rb
+++ app.rb
@@ -7,8 +7,40 @@ require 'pry'
 
 enable :sessions
 
+helpers do
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+end
 
 get '/' do
   @vents = Vent.all
   erb :index
+end
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+  user = User.find_by(email: params[:email])
+  if user && user.authenticate(params[:password])
+    # if user and password matches, create session
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    # if incorrect, go back to login page
+    redirect to '/login'
+  end
+end
+
+delete '/login' do
+  session[:user_id] = nil
+  redirect to '/login'
 end
